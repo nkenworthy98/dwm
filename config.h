a6215d5e8927be275cfe024304b56ac85e0ff30d
Nick Kenworthy <xndt98@live.com>
2021-07-16 18:27:05 -0700

    gray-blue color changes

--- config.h
+++ config.h
@@ -11,7 +11,7 @@ static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#333333";
 static const char col_gray3[]       = "#FFFFFF";
 static const char col_gray4[]       = "#FFFFFF";
-static const char col_cyan[]        = "#1793d1";
+static const char col_cyan[]        = "#2d6280";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
